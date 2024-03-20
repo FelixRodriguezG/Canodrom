33272b6974da65d262753bd7894f7a22dffe39bf
FelixRG <correofrg@gmail.com>
2024-03-20 20:08:01 +0100

    fecth and mapping data it work

--- client/src/pages/dashboards/components/CardData.tsx
+++ client/src/pages/dashboards/components/CardData.tsx
@@ -1,10 +1,4 @@
-
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useEffect, useState } from "react";
 
 interface CardItem {
@@ -19,22 +13,22 @@ function CardData() {
 
   useEffect(() => {
     // Realizar la solicitud a la base de datos utilizando fetch
-    fetch('http://localhost:3000/events/?limit=1')
-      .then(response => {
+    fetch("http://localhost:3000/events/?limit=1")
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Ocurrió un error al realizar la solicitud.');
+          throw new Error("Ocurrió un error al realizar la solicitud.");
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         setData(data);
-        console.log('Success:', data);
+        console.log("Success:", data);
         // Asignar los datos a la constante cardData
         setCardData([
           {
             id: 1,
-            title: data.title,
-            console.log(data.title)
+            title: data[0].title,
+
             value: "50",
           },
           {
@@ -53,23 +47,23 @@ function CardData() {
             value: "Trobada",
           },
         ]);
-        console.log('Success:', data);
+        console.log("Success:", data);
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
       });
-  }, [])
+  }, []);
 
   return (
-    <div className="w-[650px] h-[400px] grid grid-cols-2 gap-10 pt-36">
+    <div className="w-[500px] h-[400px] grid grid-cols-2 gap-6 pt-36 pl-9">
       {cardData.map((card) => (
         <div key={card.id}>
           <Card className="w-[100%] h-[100%] col-span-1 rounded-none shadow-lg">
             <CardHeader>
-              <CardTitle>{card.title}</CardTitle>
+              <CardTitle className="font-bold  text-xl">{card.title}</CardTitle>
             </CardHeader>
             <CardContent>
-              <p className="font-bold text-center text-2xl">{card.value}</p>
+              <p className="font-bold text-center text-xl">{card.value}</p>
             </CardContent>
           </Card>
         </div>

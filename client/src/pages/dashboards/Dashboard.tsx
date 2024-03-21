cab1e15b4c70284d6de1c9bcec07d737906bce45
Aitor <aeitor.cg@gmail.com>
2024-03-21 13:38:11 +0100

    cardData+ListComponent

--- client/src/pages/dashboards/Dashboard.tsx
+++ client/src/pages/dashboards/Dashboard.tsx
@@ -1,10 +1,13 @@
+import { ListComponent } from "./components/ListComponent"
 import CardData from "./components/CardData"
+
 
 function Dashboard() {
   return (
     <div>
       <div>
         <CardData></CardData>
+        <ListComponent></ListComponent>
         </div>
     </div>
   )

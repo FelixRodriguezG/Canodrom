107bc11a6ec65fed03bdecd9298b5e7a6adfd00b
Aitor <aeitor.cg@gmail.com>
2024-04-02 13:47:35 +0200

    funcion imprimir nombre en header implementada

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -4,10 +4,12 @@ export const loginService = async (email: string, password: string) => {
 	const response = await login(email, password);
 	console.log(response.data);
 
-	const token = response.data.accessToken;
+	const token = response.data.token;
+
+	console.log(token)
 	if (token) {
 		localStorage.setItem('user', JSON.stringify(response.data));
-
+	
 	}
 
 	return response.data;

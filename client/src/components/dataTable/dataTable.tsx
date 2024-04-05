cf41b2884fdaa541b07e79025281c6a937d607d0
FelixRG <correofrg@gmail.com>
2024-04-05 09:18:23 +0200

    Sum and method for rendered list elements

--- client/src/components/dataTable/dataTable.tsx
+++ client/src/components/dataTable/dataTable.tsx
@@ -20,8 +20,23 @@ import {
 import { useState } from "react"
 import { Input } from "../ui/input"
 import { ScrollArea } from "../ui/scroll-area"
-import { DatePickerWithRange } from "../datePicker"
 
+export interface EventDataTable {
+  attendees: number;
+  femaleAttendees: number;
+  maleAttendees: number;
+  nonBinaryAttendees: number;
+  undisclosedAttendees: number;
+  heardThroughTwitter: number;
+  heardThroughFacebook: number;
+  heardThroughInstagram: number;
+  heardThroughMastodon: number;
+  heardThroughNewsletter: number;
+  heardThroughWeb: number;
+  heardThroughSigns: number;
+  heardThroughOther: number;
+  // ... any other properties that you need
+}
 
 export interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -51,6 +66,42 @@ export function DataTable<TData, TValue>({
     },
   });
 
+  
+  const totals = table.getRowModel().rows.reduce(
+    (acc, row) => {
+      acc.attendees += row.original.attendees;
+      acc.femaleAttendees += row.original.femaleAttendees;
+      acc.maleAttendees += row.original.maleAttendees;
+      acc.nonBinaryAttendees += row.original.nonBinaryAttendees;
+      acc.undisclosedAttendees += row.original.undisclosedAttendees;
+      acc.heardThroughTwitter += row.original.heardThroughTwitter;
+      acc.heardThroughFacebook += row.original.heardThroughFacebook;
+      acc.heardThroughInstagram += row.original.heardThroughInstagram;
+      acc.heardThroughMastodon += row.original.heardThroughMastodon;
+      acc.heardThroughNewsletter += row.original.heardThroughNewsletter;
+      acc.heardThroughWeb += row.original.heardThroughWeb;
+      acc.heardThroughSigns += row.original.heardThroughSigns;
+      acc.heardThroughOther += row.original.heardThroughOther;
+      return acc;
+    },
+    {
+      attendees: 0,
+      femaleAttendees: 0,
+      maleAttendees: 0,
+      nonBinaryAttendees: 0,
+      undisclosedAttendees: 0,
+      heardThroughTwitter: 0,
+      heardThroughFacebook: 0,
+      heardThroughInstagram: 0,
+      heardThroughMastodon: 0,
+      heardThroughNewsletter: 0,
+      heardThroughWeb: 0,
+      heardThroughSigns: 0,
+      heardThroughOther: 0,
+    }
+  );
+  
+  console.log(totals);
   return (
     <>
       <div className="rounded-md border  border-gray-300 w-[98vw]">

a0a9fa13f042ff7f46a1833965fb0ded3a622924
FelixRG <correofrg@gmail.com>
2024-04-09 22:21:31 +0200

    the last commit

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,26 +7,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
-
-
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mudapp-mudapp.a.aivencloud.com',
-      port: 22116,
-      username: 'avnadmin',
-      password: 'AVNS_hrKqpPiiy920OvJoLs_',
-      database: 'cano',
-      entities: [User, Events],
-      synchronize:false,
-    }),
-    UserModule,
-    EventsModule,
-    AuthModule,
-  ],
-    controllers: [AppController],
-  providers: [AppService],
+imports: [
+ConfigModule.forRoot({
+envFilePath: '.env',
+isGlobal: true,
+}),
+TypeOrmModule.forRoot({
+type: process.env.DB_TYPE as any,
+host: process.env.DB_HOST,
+port: parseInt(process.env.DB_PORT),
+username: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_DATABASE,
+entities: [User, Events],
+synchronize: process.env.DB_SYNCHRONIZE === 'true',
+}),
+UserModule,
+EventsModule,
+AuthModule,
+],
+controllers: [AppController],
+providers: [AppService],
 })
 export class AppModule {}
